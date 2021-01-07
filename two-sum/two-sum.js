5473c22e3ac75c58455e2b60e681b6008c6756e5
Victor Scholz <64430496+victorscholz@users.noreply.github.com>
2021-01-07 18:55:13 -0500

    Time: 76 ms (85.54%), Space: 38.6 MB (75.95%) - LeetHub

---
+++ two-sum/two-sum.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+const twoSum = function(nums, target) {
+  const collection = {};
+  for(let i = 0; i < nums.length; i++){
+    if(collection[nums[i]] >= 0){
+      return [collection[nums[i]], i]
+    }
+    collection[target - nums[i]] = i
+  }
+};
+​
+​
