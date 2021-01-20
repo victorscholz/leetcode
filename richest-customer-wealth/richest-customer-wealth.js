79da216d249dc6634445322280764c605ad77c72
Victor Scholz <64430496+victorscholz@users.noreply.github.com>
2021-01-20 17:16:23 -0500

    Time: 80 ms (56.90%), Space: 38.7 MB (57.81%) - LeetHub

---
+++ richest-customer-wealth/richest-customer-wealth.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[][]} accounts
+ * @return {number}
+ */
+var maximumWealth = function(accounts) {
+  let maxWealth = 0;
+​
+  for (let i = 0; i < accounts.length; i++) {
+    let currentWealth = 0;
+    for (let j = 0; j < accounts[i].length; j++) {
+      currentWealth += accounts[i][j];
+    }
+    maxWealth = Math.max(maxWealth, currentWealth);
+  }
+  return maxWealth;
+};
