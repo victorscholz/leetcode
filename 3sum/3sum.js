dd515e666021e8c7b459a242075413f632f91d60
Victor Scholz <64430496+victorscholz@users.noreply.github.com>
2021-02-02 14:34:19 -0500

    Time: 264 ms (35.16%), Space: 49.3 MB (40.73%) - LeetHub

---
+++ 3sum/3sum.js
@@ -0,0 +1,31 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var threeSum = function(nums) {
+  nums.sort((a,b) => a - b);
+  const triplets = [];
+  
+  for(let i = 0; i < nums.length - 2; i++) {
+    if (nums[i] != nums[i - 1]) {
+      let left = i + 1;
+      let right = nums.length - 1;
+      
+      while (left < right) {
+        const currentSum = nums[i] + nums[left] + nums[right];
+        if (currentSum === 0){
+          triplets.push([nums[i], nums[left], nums[right]]);
+          while(nums[left] == nums[left + 1]) left++
+          while(nums[right] == nums[right - 1]) right--
+          left++;
+          right--;
+        } else if (currentSum < 0) {
+          left++
+        } else if (currentSum > 0) {
+          right--
+        }
+      }
+    }
+  }
+  return triplets;
+};
